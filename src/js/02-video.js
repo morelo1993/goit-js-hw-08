8567e1fa011aa7c1dd857a01043d8a70e923586b
morelo1993 <yordanimoreloperez@gmail.com>
2023-07-10 15:39:06 -0500

    Tarea8 JS terminada y Corregida

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,40 +1,20 @@
+import Vimeo from '@vimeo/player';
 import { throttle } from 'lodash';
-import Player from '@vimeo/player';
 
-let frame = document.getElementById('vimeo-player');
-let player = new Player(frame);
+const playerElem = document.getElementById("vimeo-player");
+const player = new Vimeo(playerElem);
 
-function play(data) {
-    localStorage.setItem('reproductor', data.seconds);
-    // console.log(data);
-}
+player.on(
+    "timeupdate",
+    throttle(() => {
+        player.getCurrentTime()
+            .then(currentTime => {
+                localStorage.setItem("videoplayer-current-time", currentTime);
+            });
+    }, 1000)
+);
 
-player
-    .setCurrentTime(localStorage.getItem('reproductor'))
-    .then(function (time) {
-        // console.log(time);
-        time = localStorage.getItem('reproductor');
-    });
-
-player.on('timeupdate', throttle(play, 1000));
-
-
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const vimeoPlayer = new Player(document.getElementById('vimeo-player'));
-
-//     function savePlaybackTime() {
-    //         vimeoPlayer.getCurrentTime().then((time) => {
-    //         localStorage.setItem('videoPlaybackTime', time);
-//         });
-//     }
-
-//     vimeoPlayer.on('timeupdate', throttle(savePlaybackTime, 1000));
-
-//     const savedTime = localStorage.getItem('videoPlaybackTime');
-//     if (savedTime) {
-//         vimeoPlayer.setCurrentTime(savedTime);
-//     }
-// });
+const savedTime = localStorage.getItem("videoplayer-current-time");
+savedTime === null
+    ? player.setCurrentTime(0)
+    : player.setCurrentTime(savedTime);
